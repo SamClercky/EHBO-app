fdc604f24ebee01378610e9ae9fead369c7326b3
Wim Declerck <Wim Declerck>
2017-01-07 11:46:53 +0100

    Een eerste versie van back() en forward()

--- js/index.js
+++ js/index.js
@@ -1,64 +1,64 @@
 var stap = ["ui-accordion-tabs-panel-0"];
-        var counter = 0;
+var counter = 0;
 
-        function activated(evt, ui) {
-            counter++;
-            stap[counter] = ui.newPanel.attr("id");
-             for (var i = stap.length - 1; i > counter; i--) {
-                 stap.pop();
-             }
-             disableCheck();
-        }
-        function configure(select) {
-            $(select).accordion({
-                animate: 200,
-                active: parseInt(stap[0].substr(stap[0].length -1, 1)),
-                activate: activated
-            });
-        }
-        function disableCheck() {
-            console.log("disableCheck is aangeroepen");
-            var button;
-            button = $(".terug");
-            console.log(counter == 0);
-            if (counter == 0)
-                button.attr("disabled", "disabled");
-            else
-                button.attr("disabled", false);
-            button = $(".volgende");
-            console.log(counter >= stap.length);
-            if (counter >= stap.length-1)
-                button.attr("disabled", "disabled");
-            else
-                button.attr("disabled", false);
-        }
-        function navTo(id) {
-            $("#tabs").accordion("option", "active", id);
-        }
-        function nav(teken) {
-            switch(teken) {
-                case "-":
-                    if (counter > 0) {
-                        counter--;
-                    }
-                    disableCheck();
-                    navTo(parseInt(stap[0].substr(stap[0].length -1, 1)));
-                    break;
-                case "+":
-                    if (counter < stap.length -1) {
-                        counter++;
-                    }
-                    disableCheck();
-                    navTo(parseInt(stap[0].substr(stap[0].length -1, 1)));
-                    break;
+function activated(evt, ui) {
+    counter++;
+    stap[counter] = ui.newPanel.attr("id");
+    for (var i = stap.length - 1; i > counter; i--) {
+        stap.pop();
+    }
+    disableCheck();
+}
+function configure(select) {
+    $(select).accordion({
+        animate: 200,
+        active: parseInt(stap[0].substr(stap[0].length - 1, 1)),
+        activate: activated
+    });
+}
+function disableCheck() {
+    console.log("disableCheck is aangeroepen");
+    var button;
+    button = $(".terug");
+    console.log(counter == 0);
+    if (counter == 0)
+        button.attr("disabled", "disabled");
+    else
+        button.attr("disabled", false);
+    button = $(".volgende");
+    console.log(counter >= stap.length);
+    if (counter >= stap.length - 1)
+        button.attr("disabled", "disabled");
+    else
+        button.attr("disabled", false);
+}
+function navTo(id) {
+    $("#tabs").accordion("option", "active", id);
+}
+function nav(teken) {
+    switch (teken) {
+        case "-":
+            if (counter > 0) {
+                counter--;
             }
-        }
-        $(window).on("load", function() {
-            configure("#tabs");
-            $(".terug").on("click", function() {
-                nav("-");
-            });
-            $(".volgende").on("click", function() {
-                nav("+");
-            })
-        });
+            disableCheck();
+            navTo(parseInt(stap[0].substr(stap[0].length - 1, 1)));
+            break;
+        case "+":
+            if (counter < stap.length - 1) {
+                counter++;
+            }
+            disableCheck();
+            navTo(parseInt(stap[0].substr(stap[0].length - 1, 1)));
+            break;
+    }
+}
+$(window).on("load", function () {
+    configure("#tabs");
+    $(".terug").on("click", function () {
+        nav("-");
+    });
+    $(".volgende").on("click", function () {
+        nav("+");
+    })
+});
--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,54 @@
 $.mobile.defaultPageTransition = "slide";
+var pointer = 0;
+
+// nog in bèta fase
+function navTo(id) {
+    $(id).pagecontainer("change");
+}
+
+function onStart(){
+    pointer = window.history.length;
+}
+
+function canGoBack() {
+    if (history.length > 2 || document.referrer.length > 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function canGoForward() {
+    if (pointer >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function disableCheck() {
+    if (canGoBack())
+        $(".terug").attr("disabled", false);
+    else
+        $(".terug").attr("disabled", "disabled");
+
+    if (canGoForward())
+        $(".volgende").attr("disabled", false);
+    else
+        $(".volgende").attr("disabled", "disabled");
+}
+
+$(window).on("load", function () {
+    onStart();
+    $(".terug").on("click", function () {
+        window.history.back();
+        pointer++;
+    });
+    $(".volgende").on("click", function () {
+        window.history.forward();
+        pointer--;
+    });
+    $("a").on("click", function() {
+        disableCheck();
+    });
+});
